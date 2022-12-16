d1e16e4ec0afbf38b0c78611c87ca15a58c8b9ef
Navya <navya102@gmail.com>
2022-12-16 14:01:22 +0530

    key changed

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                         = "ami-09d3b3274b6c5d4aa" #Ubuntu 20.04 LTS Free Tier Image
   instance_type               = "t3.micro"
-  key_name                    = "navya"
+  key_name                    = "divya"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
   user_data                   = file("nginx-install.sh")
