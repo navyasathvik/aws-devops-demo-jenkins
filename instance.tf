e81615518ef826c8df3f1c77a1d78bbca5f9ae66
Navya <navya102@gmail.com>
2022-12-16 14:29:02 +0530

    modified instance.tf file

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-09d3b3274b6c5d4aa" #Ubuntu 20.04 LTS Free Tier Image
-  instance_type               = "t3.micro"
+  ami                         = "amiami-0b0dcb5067f052a63" #Ubuntu 20.04 LTS Free Tier Image
+  instance_type               = "t3.medium"
   key_name                    = "divya"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
